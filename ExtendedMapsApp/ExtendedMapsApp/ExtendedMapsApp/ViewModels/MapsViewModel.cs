5bbd5832f7dd9b42e37b43def263499ea1176255
Sebastian Jensen <apps@tsjdev-apps.de>
2018-02-10 11:40:27 +0100

    - add mapsviewmodel

---
+++ ExtendedMapsApp/ExtendedMapsApp/ExtendedMapsApp/ViewModels/MapsViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using Xamarin.Forms.Maps;
+
+namespace ExtendedMapsApp.ViewModels
+{
+    public class MapsViewModel : INotifyPropertyChanged
+    {
+        private List<Pin> _pins;
+        public List<Pin> Pins
+        {
+            get => _pins;
+            set { _pins = value; OnPropertyChanged(); }
+        }
+
+        public MapsViewModel()
+        {
+            Pins = new List<Pin>
+            {
+                new Pin { Position = new Position(41.902916, 12.453389), Address = "Italien", Label = "Vatikanstadt" }
+            };
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
